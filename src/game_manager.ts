90b31f0d3e921a26391948cbbbd7414e8686b268
mharm-msft <miharmon@microsoft.com>
2026-09-09 07:31:23 -0400

    Increase the number of starting tiles

--- src/game_manager.ts
+++ src/game_manager.ts
@@ -16,7 +16,7 @@ export class GameManager {
   /** Game State */
   static state: GameState
   /** Start Tiles Count */
-  static startTiles: number = 1
+  static startTiles: number = 4
 
   constructor(size: number) {
     GameManager.size = size
@@ -118,7 +118,7 @@ export class GameManager {
    */
   static addRandomTile(): void {
     if (Grid.cellsAvailable()) {
-      const value = Math.random() < 0.9 ? 2 : 4
+      const value = Math.random() < 0.5 ? 2 : 4
       const cell = Grid.randomAvailableCell()
 
       if (cell !== null) Grid.insertTile(new Tile(cell, value))
