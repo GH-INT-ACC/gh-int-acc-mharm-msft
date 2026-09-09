9a2e00ba997b337d29d7dd437fd65ab807f7be4f
mharm-msft <miharmon@microsoft.com>
2026-09-09 07:31:05 -0400

    Adding unit tests 1

--- __tests__/keyboard_input_manager.test.ts
+++ __tests__/keyboard_input_manager.test.ts
@@ -1,13 +1,21 @@
 import { jest } from '@jest/globals'
 
+const { KeyboardInputManager } = await import(
+  '../src/keyboard_input_manager.js'
+)
+
 describe('KeyboardInputManager', () => {
   afterEach(() => {
     jest.resetAllMocks()
   })
 
-  describe('todo', () => {
-    it('Create some unit tests', async () => {
-      expect(true).toBe(true)
+  describe('on()', () => {
+    it('Registers an event listener', async () => {
+      KeyboardInputManager.events['test'] = []
+
+      KeyboardInputManager.on('test', () => {})
+
+      expect(KeyboardInputManager.events['test'].length).toBe(1)
     })
   })
 })
